b1bf941b0e31299b75e54fd757da63efbe0cfaf5
ServerTab <162655398+ServerTab@users.noreply.github.com>
2024-09-29 12:25:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 require('module-alias/register')
 const path = require('path')
 const Bitzxier = require(`./structures/Bitzxier.js`)
-const client = new Bitzxier()
+const client = new client()
 this.config = require(`${process.cwd()}/config.json`);
 (async () => {
     await client.initializeMongoose()
